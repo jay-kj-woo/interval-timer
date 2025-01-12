00d4c81c2e55292ee8e20dea24dc69d79b90a62d
Jay Woo <jw_1020@hotmail.com>
2025-01-11 19:40:52 -0700

    adds local storage service client

---
+++ src/utils/timerStorageClient.ts
@@ -0,0 +1,40 @@
+import {
+  CreateIntervalTimer,
+  DeleteIntervalTimer,
+  IntervalTimer,
+  UpdateIntervalTimer,
+} from '../types/IntervalTimer';
+
+export class TimerStorageClient {
+  static getTimers(): IntervalTimer[] {
+    return JSON.parse(localStorage.getItem('timers') || '[]');
+  }
+
+  static saveTimer(timer: CreateIntervalTimer) {
+    const id = this.generateId();
+    const newTimer = { ...timer, id };
+    const timers = this.getTimers();
+    timers.push(newTimer);
+    localStorage.setItem('timers', JSON.stringify(timers));
+  }
+
+  static updateTimer(timer: UpdateIntervalTimer) {
+    const timers = this.getTimers();
+    const index = timers.findIndex((t) => t.id === timer.id);
+    if (index === -1) throw new Error('Timer not found');
+    timers[index] = { ...timers[index], ...timer };
+    localStorage.setItem('timers', JSON.stringify(timers));
+  }
+
+  static deleteTimer(timer: DeleteIntervalTimer) {
+    const timers = this.getTimers();
+    const index = timers.findIndex((t) => t.id === timer.id);
+    if (index === -1) throw new Error('Timer not found');
+    timers.splice(index, 1);
+    localStorage.setItem('timers', JSON.stringify(timers));
+  }
+
+  static generateId(): number {
+    return this.getTimers().length + 1;
+  }
+}
