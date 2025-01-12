e90f164bdf216792c87e01b813b23ea76aecb56b
Jay Woo <jw_1020@hotmail.com>
2025-01-11 21:12:49 -0700

    creates different Timer interfaces

---
+++ src/components/TimerForm.tsx
@@ -0,0 +1,127 @@
+import { PlusIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import {
+  CreateIntervalTimer,
+  IntervalTimer,
+  UpdateIntervalTimer,
+} from '../types/IntervalTimer';
+
+type Props =
+  | {
+      existingConfig: IntervalTimer;
+      onCancel: () => void;
+      onSave: (newConfig: UpdateIntervalTimer) => void;
+      type: 'edit';
+    }
+  | {
+      onCancel: () => void;
+      onSave: (newConfig: CreateIntervalTimer) => void;
+      type: 'create';
+    };
+
+const TimerForm = (props: Props) => {
+  const { onCancel, onSave, type } = props;
+  const [newConfig, setNewConfig] = useState<
+    CreateIntervalTimer | UpdateIntervalTimer
+  >(
+    type === 'edit'
+      ? props.existingConfig
+      : {
+          name: '',
+          highIntensity: 0,
+          lowIntensity: 0,
+          rounds: 0,
+        }
+  );
+
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setNewConfig((prevConfig) => ({
+      ...(prevConfig || {}),
+      [target.name]: target.value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (type === 'edit') {
+      onSave(newConfig as UpdateIntervalTimer);
+    } else {
+      onSave(newConfig as CreateIntervalTimer);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-4 w-80">
+        <div className="flex portrait:flex-col landscape:flex-row gap-2">
+          <label htmlFor="name" className="text-slate-800">
+            Name
+          </label>
+          <input
+            className="border border-purple-800 rounded-md p-1 pl-4"
+            type="text"
+            value={newConfig?.name}
+            name="name"
+            required
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="flex portrait:flex-col landscape:flex-row gap-2">
+          <label htmlFor="highIntensity">High Intensity</label>
+          <input
+            className="border border-purple-800 rounded-md p-1 pl-4"
+            type="number"
+            value={newConfig?.highIntensity}
+            name="highIntensity"
+            min={1}
+            required
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="flex portrait:flex-col landscape:flex-row gap-2">
+          <label htmlFor="lowIntensity">Low Intensity</label>
+          <input
+            className="border border-purple-800 rounded-md p-1 pl-4"
+            type="number"
+            value={newConfig?.lowIntensity}
+            name="lowIntensity"
+            required
+            min={1}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="flex portrait:flex-col landscape:flex-row gap-2">
+          <label htmlFor="rounds">Rounds</label>
+          <input
+            className="border border-purple-800 rounded-md p-1 pl-4"
+            type="number"
+            value={newConfig?.rounds}
+            name="rounds"
+            min={1}
+            required
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="flex items-center justify-between gap-4 mt-4">
+          <button
+            onClick={onCancel}
+            className="flex items-center gap-2
+          border border-purple-800 rounded-md px-4 py-2"
+          >
+            <XMarkIcon className="w-4 h-4" />
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="flex items-center gap-2
+          border border-yellow-400 bg-yellow-400 rounded-md px-4 py-2"
+          >
+            <PlusIcon className="w-4 h-4" />
+            Add Timer
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default TimerForm;
--- src/types/IntervalTimer.ts
+++ src/types/IntervalTimer.ts
@@ -3,4 +3,20 @@ export interface IntervalTimer {
   lowIntensity: number;
   rounds: number;
   name: string;
+  id: number;
+}
+
+export interface CreateIntervalTimer {
+  highIntensity: number;
+  lowIntensity: number;
+  rounds: number;
+  name: string;
+}
+
+export interface UpdateIntervalTimer extends Partial<CreateIntervalTimer> {
+  id: number;
+}
+
+export interface DeleteIntervalTimer {
+  id: number;
 }
